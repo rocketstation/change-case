76dc3e2a388e53815b7ab7194b18f84482cf7931
oleksiibilous <oleksiibilous@rstation.io>
2018-03-29 17:18:48 +0300

    change test

--- src/index.test.js
+++ src/index.test.js
@@ -6,39 +6,44 @@ import {
   upperFirst,
 } from 'lodash'
 
-import * as changeCase from './index'
+import {
+  cl,
+  cu,
+  sl,
+  su,
+  k,
+} from './index'
 
 jest.mock('lodash')
 
-
 it('changes to camel lower case', () => {
-  changeCase.cl()
+  cl()
 
   expect(camelCase).toBeCalled()
 })
 
 it('changes to camel upper case', () => {
-  changeCase.cu()
+  cu()
 
   expect(camelCase).toBeCalled()
   expect(upperFirst).toBeCalled()
 })
 
 it('changes to snake lower case', () => {
-  changeCase.sl()
+  sl()
 
   expect(snakeCase).toBeCalled()
 })
 
 it('changes to snake upper case', () => {
-  changeCase.su()
+  su()
 
   expect(snakeCase).toBeCalled()
   expect(toUpper).toBeCalled()
 })
 
 it('changes to kebab case', () => {
-  changeCase.k()
+  k()
 
   expect(kebabCase).toBeCalled()
 })
